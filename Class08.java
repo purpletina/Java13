0ea9052b0e2bd5dfa95d065904587870d6bea549
purpletina <113041666+purpletina@users.noreply.github.com>
2022-12-14 01:04:40 +0800

    Update Class08.java

--- Class08.java
+++ Class08.java
@@ -1,1 +1,19 @@
+package Java13;
 
+class Ctest {
+	public static void test(int num, int den) throws ArithmeticException {
+		System.out.println(num + "/" + den + "=" + num / den);
+	}
+
+	public class class08 {
+		public static void main(String[] args) {
+			try {
+				test(5, 0);
+			} catch (ArithmeticException e) {
+				System.out.println("除數為 0");
+			} finally {
+				System.out.println("end of main() method!!");
+			}
+		}
+	}
+}
