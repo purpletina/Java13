a31ba882fdc72c9f118f65890c03f30de34e99cc
purpletina <113041666+purpletina@users.noreply.github.com>
2022-12-14 00:58:16 +0800

    Update Class06.java

--- Class06.java
+++ Class06.java
@@ -1,1 +1,16 @@
+package Java13;
 
+public class class06 {
+	public static void main(String[] args) {
+		try {
+			int num = 12;
+			int den[] = { 12, 0, 3, 0, 0, 4 };
+			for (int i = 0; i < 10; i++) {
+				System.out.println("ans=" + num / den[i]);
+			}
+		} catch (Exception e) {
+			System.out.println("捕捉到例外了");
+
+		}
+	}
+}
