1a99d7dcbc0d2bbe75f0d83f6fc9783969668678
purpletina <113041666+purpletina@users.noreply.github.com>
2022-12-14 01:02:11 +0800

    Update Class07.java

--- Class07.java
+++ Class07.java
@@ -1,1 +1,17 @@
-
+package Java13;
+public class class07 {
+	public static void test(int num, int den)throws ArithmeticException{
+		System.out.println(num+"/"+den+"="+num/den);
+	}
+	public static void main(String[] args) {
+		try {
+			test(5,0);
+		}
+		catch(ArithmeticException e) {
+			System.out.println("除數為 0");
+		}
+		finally {
+			System.out.println("end of main() method!!");
+		}
+	}
+}
