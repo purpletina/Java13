c33ad8523396950f0db35246f47d66e552a836e7
purpletina <113041666+purpletina@users.noreply.github.com>
2022-12-14 00:29:17 +0800

    Update Class03.java

--- Class03.java
+++ Class03.java
@@ -1,1 +1,16 @@
-
+package Java13;
+public class class03 {	
+	public static void main(String[] args) {
+		try {
+			int num=12, den = 0;
+			int ans = num/den;
+			System.out.println("ans="+ans);
+		}
+		catch(ArithmeticException e) {
+			System.out.println("除數為0");
+		}
+		finally {
+			System.out.println("end of main() method!!");
+		}
+	}
+}
