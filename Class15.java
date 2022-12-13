c2500ab4379c91e9b1de8135a34f4d6e4ce1481d
purpletina <113041666+purpletina@users.noreply.github.com>
2022-12-14 01:37:29 +0800

    Update Class15.java

--- Class15.java
+++ Class15.java
@@ -1,1 +1,30 @@
-
+package Java13;
+import java.io.*;
+import java.math.*;
+class ArgumentOutOfBound extends Exception{
+}
+public class class15 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader buf;
+		String str;
+		int num;
+		
+		buf=new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("請輸入大於 0 的整數:");
+		str=buf.readLine();
+		num=Integer.parseInt(str);
+		mySqrt(num);
+	}
+	public static void mySqrt(int n) {
+		try {
+			if(n<0) {
+				throw new ArgumentOutOfBound();
+			}else {
+				System.out.println("sqrt("+n+")="+Math.sqrt(n));
+			}
+		}
+		catch(ArgumentOutOfBound e) {
+			System.out.println(n+"小於 0");
+		}		
+	}
+}
